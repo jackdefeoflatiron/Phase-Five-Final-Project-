f3a1b718cac1da37a2ae1a8a66cf6a6d515965e6
Jack DeFeo <jackd3243@gmail.com>
2022-02-03 11:08:41 -0500

    backend work

--- Phase-Five-Final-Project-/app/controllers/users_controller.rb
+++ Phase-Five-Final-Project-/app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
     
+    def index 
+        users = User.all
+        render json: users 
+    end
+        
+    def show
+        render json: @current_user, status: :accepted
+    end
+        
+    
     def create 
-        user = User.create!(user_params)
+        parameters = user_params.to_h
+        if parameters[:profile_picture].blank? then 
+            parameters.delete(:profile_picture)
+        end
+
+        user = User.create!(parameters)
+        session[:user_id] = user.id
+        render json: user, status: :created 
         
     end 
+    private 
+    def user_params
+        params.permit(:username, :password, :bio, :admin, :profile_picture, :email_address)
+    end 
+
 end
