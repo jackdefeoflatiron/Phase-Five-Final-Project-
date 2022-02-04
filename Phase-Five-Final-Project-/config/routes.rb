2d18c71d27ac6553a70bbfbefdc0c026063d0c39
Jack DeFeo <jackd3243@gmail.com>
2022-02-04 10:58:04 -0500

    backend work

--- Phase-Five-Final-Project-/config/routes.rb
+++ Phase-Five-Final-Project-/config/routes.rb
@@ -9,4 +9,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  patch "/users", to: "users#update"
+  #SESSIONS
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
