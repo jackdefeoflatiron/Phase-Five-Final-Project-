f3be58b4bc06afd0f2f8e8c10d39a8df4d921bbb
Jack DeFeo <jackd3243@gmail.com>
2022-02-15 13:44:18 -0500

    Creating the Front Endexit

--- Phase-Five-Final-Project-/client/src/components/StoryDisplay.js
+++ Phase-Five-Final-Project-/client/src/components/StoryDisplay.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import {BrowserRouter, Switch, Route, Link, useParams} from 'react-router-dom'
 
 function StoryDisplay({login,user, story}) {
-
+const [selectedStory, setSelectedStory] = useState([])
 // const [chapter, setChapter] = useState([])
 
 // useEffect(() => {
@@ -10,29 +10,30 @@ function StoryDisplay({login,user, story}) {
 // .then((res)=> res.json())
 // .then((data) => setChapter(data))
 // }, [])
-// // console.log(story)
+console.log(story)
 const params = useParams()
 
 console.log(params)
 
-const displaySelectedStory = story.filter((story) => {
-    
+const selectedStory = story.filter(story => {
+if (story.id === params.id)
+return setSelectedStory(story)
 })
 
-}
+console.log(selectedStory.author)
 
 return(
     <div>
         <Link to='/home'>Main Page</Link>
-        <h1>{params?.story_name}</h1>
-            <p>{params?.author}</p>
-            <p>{story?.age_group}</p>
-            <p>{story?.genre}</p>
-            <p>{story?.style}</p>
+        <h1>{selectedStory?.story_name}</h1>
+            <p>{selectedStory?.author}</p>
+            <p>{selectedStory?.age_group}</p>
+            <p>{selectedStory?.genre}</p>
+            <p>{selectedStory?.style}</p>
             <br></br>
-            <p>{story?.title}</p>
-            <p>{story?.chapter_number}</p>
-            <p>{story?.body}</p>
+            <p>{selectedStory?.title}</p>
+            <p>{selectedStory?.chapter_number}</p>
+            <p>{selectedStory?.body}</p>
     </div>
 )
 }
