1a28e04909bf917afa35c59f2094a0257b313893
Jack DeFeo <jackd3243@gmail.com>
2022-02-24 15:52:26 -0500

    Working on Crud mvps

--- Phase-Five-Final-Project-/client/src/components/StoryPreview.js
+++ Phase-Five-Final-Project-/client/src/components/StoryPreview.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import StoryDisplayer from './StoryDisplay'
-import {BrowserRouter, Switch, Route,  Link } from 'react-router-dom'
+import {BrowserRouter, Switch, Route,  Link,  useParams, useHistory} from 'react-router-dom'
 
 
 function StoryPreview({allStories, user, login}) {
 
+const id = allStories.user_id
+const params = useParams();
+const history = useHistory();
 console.log(allStories)
     return(
         <div>
@@ -12,7 +15,7 @@ console.log(allStories)
         <l1>
             <Link allStories={allStories} to={`/story/${allStories.id}`}> {allStories.story_name}</Link>
             <br/>
-            <Link user={user} allStories={allStories} to={`/userviewer/${allStories.user_id}`}>{allStories.author}</Link>
+            <Link user={user} allStories={allStories} to={`/userviewer/${user}`}>{allStories.author}</Link>
             <p>{allStories.age_group}</p>
             <p>{allStories.genre}</p>
             <p>{allStories.style}</p>
